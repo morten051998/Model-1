ab6a15c78d9d98cb9790bef44d60fda9fd609c2b
Morten <morten1998@live.no>
2024-10-01 23:49:04 -1000

    Initial commit

---
+++ Git test.R
@@ -0,0 +1,47 @@
+# test Github in R
+
+# check if git is installed
+
+git --version
+
+# Create token
+usethis::create_github_token()
+
+# Testing
+gitcreds::gitcreds_set()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
